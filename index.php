9e0884942cf3b8647f39f9b044bac2b2a31a0dfa
Tyler Beauchamp <tbchamp@cs.washington.edu>
2012-02-15 23:18:47 -0800

    revminer updates

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 		  	<img src="images/headline-arrow.gif" alt="arrow" />
 		    <h2>CSE Projects:</h2>
 		    <p>CSE403: <a href="http://cubist.cs.washington.edu/projects/12wi/cse403/r/index.html">Recipe Reader Andriod App</a></p>
-            <p>CSE454: RevMiner Project</p>
+            <p>CSE454: <a href="CSEprojects/revminer.html">RevMiner Project</a></p>
             <p></p>
 		    <img src="images/headline-arrow.gif" alt="arrow" />
 		    <h2>HCDE 333 Portfolio:</h2>
